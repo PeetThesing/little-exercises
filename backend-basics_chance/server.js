9db440229c98740867eb5b83d1200414e36013e6
Peet Thesing <p.thesing@posteo.de>
2023-09-18 12:23:03 +0200

    change text in chances exercise

--- backend-basics_chance/server.js
+++ backend-basics_chance/server.js
@@ -2,13 +2,22 @@ import { createServer } from "node:http";
 import Chance from "chance";
 
 const chance = new Chance();
+
 const name = chance.name();
 const age = chance.age();
 const profession = chance.profession();
+const gender = chance.gender({
+  extraGenders: ["Agender", "Genderqueer", "Trans", "Intersex"],
+});
 
 export const server = createServer((request, response) => {
-  response.statusCode = 200;
-  response.end(
-    `Hello, my name is ${name} and I am ${age} years old. I am a ${profession}.`
-  );
+  if (request.url === "/") {
+    response.statusCode = 200;
+    response.end(
+      `Hello, my name is ${name} and I am ${age} years old. I am a ${profession}. I am ${gender}, even though my name might suggest otherwise.`
+    );
+  } else {
+    response.statusCode = 404;
+    response.end("Not Found");
+  }
 });
