cbccf0ba830a2e471f492c3f5c81d3341cdb73f5
Peet Thesing <p.thesing@posteo.de>
2023-09-21 11:37:22 +0200

    add backend products mongoose

---
+++ backend-read_products/pages/api/products/[id].js
@@ -0,0 +1,24 @@
+import dbConnect from "@/db/dbConnect";
+import Product from "@/db/models/Product";
+
+export default async function handler(request, response) {
+  await dbConnect();
+  const { id } = request.query;
+
+  if (request.method === "GET") {
+    const product = await Product.findById(id);
+
+    if (!product) {
+      return response.status(404).json({ status: "Not Found" });
+    }
+    response.status(200).json(product);
+  }
+}
+
+// const product = products.find((product) => product.id === id);
+
+// if (!product) {
+//   return response.status(404).json({ status: "Not Found" });
+// }
+
+// response.status(200).json(product);
